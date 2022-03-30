dc44c5d9c4c5e7fca4e46c79bc162419ca797df4
shivam201483101 <shivam201483101@vcet.edu.in>
2022-03-30 23:24:28 +0530

    first

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,84 @@
+import { AppBar, makeStyles, Tabs , Tab } from "@material-ui/core";
+import React from "react";
+import { sortingAlgorithms } from "../common/config";
+import { useData } from "../common/store";
+import shallow from "zustand/shallow";
+import { AiFillGithub } from "react-icons/ai";
+import MotionPhotosAutoTwoToneIcon from '@mui/icons-material/MotionPhotosAutoTwoTone';
+import { createTheme } from '@mui/material/styles';
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    "aria-controls": `scrollable-auto-tabpanel-${index}`,
+  };
+}
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export function NavBar() {
+  const classes = useStyles();
+
+  const [algorithm, setAlgorithm] = useData(
+    (state) => [state.algorithm, state.setAlgorithm],
+    shallow
+  );
+
+  return (
+    <div className={classes.root}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <MotionPhotosAutoTwoToneIcon></MotionPhotosAutoTwoToneIcon>
+        <h3>Algo-Visualizer</h3>
+        { <a href="https://github.com/sadanandpai/sort-visualizer" target="_blank"><AiFillGithub style={{ fontSize: "1.5rem" }} /></a> }
+      </div>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={algorithm}
+          onChange={(event, id) => setAlgorithm(id)}
+          indicatorColor="primary"
+          textColor="secondary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          {sortingAlgorithms.map((algorithm) => (
+            <Tab
+            
+              label={algorithm.title}
+              {...a11yProps(0)}
+              key={algorithm.title}
+            />
+          ))}
+          <Tab label="All" {...a11yProps(6)} />
+        </Tabs>
+      </AppBar>
+    </div>
+  );
+}
